1d9c71517d53c6ac01ed232f6935e34e99bc3827
Kai Cataldo <kaicataldo@gmail.com>
2015-09-15 01:27:19 -0400

    Updated config, more aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,11 +1,23 @@
+# General
+alias c='clear'
+alias h='history'
+
 # Directory Info
-alias ll='ls -GFhl'
-alias la='ls -GFhla' # List all files (inlcuding hidden)
-alias lh='ls -GFdl .*' # List hidden files only
+alias ll='ls -lFh'
+alias la='ls -lAFh' # List all files (inlcuding hidden)
+alias lh='ls -ld .*' # List hidden files only
+alias lr='ls -tRFh' # List recursively
+
+# zsh config
+alias zshrc="${EDITOR} ${$(cd -P $(dirname $0) && cd ..; pwd -P)}"
+alias reload=". ~/.zshrc && echo 'Shell config reloaded from ~/.zshrc'"
+
+# Homebrew
+alias brewup='brew update && brew upgrade --all && brew cleanup && brew prune && brew doctor'
+
+# npm
+alias npmls='npm ls --depth=0'
 
 # Finder
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
-
-# Homebrew
-alias brewup='brew update && brew upgrade --all && brew cleanup && brew prune && brew doctor'
