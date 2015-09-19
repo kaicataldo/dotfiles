77ae6997b97f9112602b0bd9fa43b94af4e69e37
Kai Cataldo <kaicataldo@gmail.com>
2015-09-18 21:46:02 -0400

    Add localrc alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -8,9 +8,12 @@ alias la='ls -lAFh' # List all files (inlcuding hidden)
 alias lh='ls -ld .*' # List hidden files only
 alias lr='ls -tRFh' # List recursively
 
-# zsh config
+# zshrc config
 alias zshrc="${EDITOR} ${$(cd -P $(dirname $0) && cd ..; pwd -P)}"
 alias reload=". ~/.zshrc && echo 'Shell config reloaded from ~/.zshrc'"
+
+#localrc config
+alias localrc="if [[ -a ~/.localrc ]]; then ${EDITOR} ~/.localrc; fi"
 
 # Homebrew
 alias brewup='brew update && brew upgrade --all && brew cleanup && brew prune && brew doctor'
