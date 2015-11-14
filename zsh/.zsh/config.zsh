8cf801aa8c1e361c68eb03e5f5ff2c3fdd226171
Kai Cataldo <kaicataldo@gmail.com>
2015-11-14 02:04:31 -0500

    Zsh Vim key bindings

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -17,3 +17,7 @@ setopt hist_ignore_all_dups # Don't save duplicates
 
 # Expansion and Globbing
 setopt extended_glob # treat #, ~, and ^ as part of patterns for filename generation
+
+# Key bindings
+bindkey -v # Automatically set if EDITOR contains 'vi'
+bindkey jj vi-cmd-mode
