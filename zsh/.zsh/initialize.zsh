111a6155c6c5942f9faa21379f6f0da90fb801ee
Kai Cataldo <kaicataldo@gmail.com>
2016-08-12 12:03:43 -0400

    Add fzf

--- zsh/.zsh/initialize.zsh
+++ zsh/.zsh/initialize.zsh
@@ -11,19 +11,24 @@ prompt pure
 # z
 . $ZSH/vendor/z/z.sh
 
+# fzf
+if [ -f ~/.fzf.zsh ]; then
+  source ~/.fzf.zsh
+fi
+
 # Homebrew
 export PATH="/usr/local/sbin:$PATH"
 
 # nvm
 export NVM_DIR=$HOME/.nvm
 
-if [[ -s $NVM_DIR/nvm.sh ]]; then
+if [ -s $NVM_DIR/nvm.sh ]; then
   . $NVM_DIR/nvm.sh
 fi
 
 # rbenv
-if [[ -s $HOME/.rbenv ]]; then
-  if [[ $OSTYPE == "linux-gnu" ]]; then
+if [ -s $HOME/.rbenv ]; then
+  if [ $OSTYPE == "linux-gnu" ]; then
     export PATH=$HOME/.rbenv/bin:$PATH
   fi
 
@@ -34,8 +39,3 @@ fi
 export GOPATH=$HOME/Projects/go
 export PATH=$GOPATH/bin:$PATH
 export PATH=$PATH:/usr/local/opt/go/libexec/bin
-
-# Archey
-if [[ $OSTYPE == darwin* && -s /usr/local/bin/archey ]]; then
-  archey --offline
-fi
