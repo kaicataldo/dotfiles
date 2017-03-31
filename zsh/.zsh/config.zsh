214159438e2314d64b728f4acf7d08c60264810d
Kai Cataldo <kaicataldo@gmail.com>
2017-03-31 12:18:33 -0400

    Zsh: Remove setopt inc_append_history

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -9,7 +9,6 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt inc_append_history # Append history as commands are executed
 setopt share_history # Share history across sessions
 setopt hist_ignore_all_dups # Don't save duplicates
 
